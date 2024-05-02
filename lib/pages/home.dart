1d5ab97902f3a7ac4c4c734340445ed0ef8eac0a
MohamedJmal99 <Mohamedjmal73@gmail.com>
2024-05-02 22:44:11 +0100

    Completed Home Page !

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,68 +14,266 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffedf3f6),
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-          Stack(
-            children: [Container(
-              height: 220,
-              padding: EdgeInsets.only(left: 20.0, top: 50.0),
-              decoration: BoxDecoration(
-                color: Color(0xFF2a2b31), borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50),bottomRight: Radius.circular(50)),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                ClipRRect(
-                    borderRadius: BorderRadius.circular(60),
-                    child: Image.asset("images/mohamed.jpg",height: 50,width: 50, fit: BoxFit.cover,)),
-                  SizedBox(width: 20.0,),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10.0),
-                    child: Text("Mohamed Jmal" , style:  TextStyle(color: Colors.white , fontSize: 22.0, fontWeight: FontWeight.bold)),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(bottom: 30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(children: [
+                Container(
+                  height: 220,
+                  padding: EdgeInsets.only(left: 20.0, top: 50.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF2a2b31),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(50),
+                        bottomRight: Radius.circular(50)),
                   ),
-              ],),
-            ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.transparent,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                margin: EdgeInsets.only(top: 120.0, left: 20.0, right: 20),
-                child: SizedBox(
-                  width: double.infinity, // Set the width to infinity to fill the parent width
-                  height: 200, // Assuming you want a fixed height for the container
                   child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Expanded(
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(50),
+                      ClipRRect(
+                          borderRadius: BorderRadius.circular(60),
                           child: Image.asset(
-                            "images/quiz-image.jpg",
-                            fit: BoxFit.fill // This will make the image fill the available space
-                          ),
-                        ),
+                            "images/mohamed.jpg",
+                            height: 50,
+                            width: 50,
+                            fit: BoxFit.cover,
+                          )),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Text("Mohamed Jmal",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.bold)),
                       ),
                     ],
                   ),
                 ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  margin: EdgeInsets.only(top: 120.0, left: 20.0, right: 20),
+                  child: SizedBox(
+                    width: double.infinity,
+                    // Set the width to infinity to fill the parent width
+                    height: 200,
+                    // Assuming you want a fixed height for the container
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: Image.asset("images/quiz-image.jpg",
+                                fit: BoxFit
+                                    .fill // This will make the image fill the available space
+                                ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ]),
+              SizedBox(
+                height: 30.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Text("Top Quiz Categories",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w900)),
+              ),
+              SizedBox(height: 20.0,),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Material(
+                      borderRadius: BorderRadius.circular(20),
+                      elevation: 5.0,
+                      child: Container(
+                          width: 150,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20)),
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                "images/place.jpg",
+                                height: 80,
+                                width: 80,
+                                fit: BoxFit.cover,
+                              ),
+                              SizedBox(height: 20.0),
+                              Text("Place",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.w500)),
+                            ],
+                          )
+                      ),
+                    ),
+                    Material(
+                      borderRadius: BorderRadius.circular(20),
+                      elevation: 5.0,
+                      child: Container(
+                          width: 150,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20)),
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                "images/animal.jpg",
+                                height: 80,
+                                width: 80,
+                                fit: BoxFit.cover,
+                              ),
+                              SizedBox(height: 20.0),
+                              Text("Animals",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.w500)),
+                            ],
+                          )
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0,),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Material(
+                      borderRadius: BorderRadius.circular(20),
+                      elevation: 5.0,
+                      child: Container(
+                          width: 150,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20)),
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                "images/fruit.jpg",
+                                height: 80,
+                                width: 80,
+                                fit: BoxFit.cover,
+                              ),
+                              SizedBox(height: 20.0),
+                              Text("Fruits",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.w500)),
+                            ],
+                          )
+                      ),
+                    ),
+                    Material(
+                      borderRadius: BorderRadius.circular(20),
+                      elevation: 5.0,
+                      child: Container(
+                          width: 150,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20)),
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                "images/object.jpg",
+                                height: 80,
+                                width: 80,
+                                fit: BoxFit.cover,
+                              ),
+                              SizedBox(height: 20.0),
+                              Text("Objects",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.w500)),
+                            ],
+                          )
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0,),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Material(
+                      borderRadius: BorderRadius.circular(20),
+                      elevation: 5.0,
+                      child: Container(
+                          width: 150,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20)),
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                "images/football.jpg",
+                                height: 80,
+                                width: 80,
+                                fit: BoxFit.cover,
+                              ),
+                              SizedBox(height: 20.0),
+                              Text("Sports",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.w500)),
+                            ],
+                          )
+                      ),
+                    ),
+                    Material(
+                      borderRadius: BorderRadius.circular(20),
+                      elevation: 5.0,
+                      child: Container(
+                          width: 150,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20)),
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                "images/quiz-box.jpg",
+                                height: 80,
+                                width: 80,
+                                fit: BoxFit.cover,
+                              ),
+                              SizedBox(height: 20.0),
+                              Text("Random",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.w500)),
+                            ],
+                          )
+                      ),
+                    )
+                  ],
+                ),
               )
-            ]
+            ],
           ),
-          SizedBox(height: 30.0,),
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0),
-              child: Text("Top Quiz Categories" , style:  TextStyle(color: Colors.black , fontSize: 24.0, fontWeight: FontWeight.w900)),
-            ),
-            Row(
-              children: [
-
-              ],
-            )
-        ],),
+        ),
       ),
     );
   }
